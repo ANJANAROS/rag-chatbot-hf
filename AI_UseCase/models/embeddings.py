b885bac96f92b40c25da8159f906998e0421a6b7
ANJANAROS <124654456+ANJANAROS@users.noreply.github.com>
2025-11-24 16:01:34 +0530

    Update embeddings.py

--- AI_UseCase/models/embeddings.py
+++ AI_UseCase/models/embeddings.py
@@ -1,9 +1,10 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from config import HF_EMBED_MODEL
+from config.config import HF_EMBED_MODEL
 
 
 
 
 def get_embedding_model():
     """Return a HuggingFaceEmbeddings instance."""
+
     return HuggingFaceEmbeddings(model_name=HF_EMBED_MODEL)
